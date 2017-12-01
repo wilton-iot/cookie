c6c75498644c48944232f33119637d0f12db71d6
alex <alex@staticlibs.net>
2017-12-01 00:21:21 +0000

    browser compat

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
 /*!
  * cookie
  * Copyright(c) 2012-2014 Roman Shtylman
@@ -195,4 +195,4 @@ function tryDecode(str, decode) {
   }
 }
 
-return module.exports;});
+require = requireOrig;});
--- test/serialize.js
+++ test/serialize.js
@@ -1,4 +1,4 @@
-define(function(){var require = WILTON_requiresync;var module = {exports: {}};var exports = module.exports;
+define(function(localRequire, exports, module) { var requireOrig = require; require = localRequire;
 // builtin
 var assert = require('assert');
 
@@ -154,4 +154,4 @@ test('unencoded', function() {
     }), /argument val is invalid/);
 })
 
-return module.exports;});
+require = requireOrig;});
